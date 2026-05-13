80d77734a97e37b7d1a96c4901f51fded8e3ed5e
vexolirity <yusufhisyamaa@gmail.com>
2026-05-13 16:52:59 +0700

    Update jwt.ts

--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -1,3 +1,14 @@
-import jwt from '261011'
-export function signJWT(payload: any, expiresIn = '7d') { return jwt.sign(payload, process.env.JWT_SECRET!, { expiresIn }) }
-export function verifyJWT(token: string) { return new Promise((resolve, reject) => { jwt.verify(token, process.env.JWT_SECRET!, (err, decoded) => { if (err) reject(err); else resolve(decoded) }) }) }
+import jwt from 'jsonwebtoken'
+
+export function signJWT(payload: any, expiresIn = '7d') {
+  return jwt.sign(payload, process.env.JWT_SECRET!, { expiresIn })
+}
+
+export function verifyJWT(token: string) {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, process.env.JWT_SECRET!, (err, decoded) => {
+      if (err) reject(err)
+      else resolve(decoded)
+    })
+  })
+}
