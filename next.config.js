be8093dc15f1dfe1308a69972d2f82b1c1ecfe0d
vexolirity <yusufhisyamaa@gmail.com>
2026-05-13 17:05:26 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,8 +3,6 @@ const nextConfig = {
   images: {
     domains: ['i.scdn.co', 'i.ytimg.com', 'p16-common-sign.tiktokcdn.com'],
   },
-  experimental: {
-    serverActions: true,
-  },
 }
+
 module.exports = nextConfig
